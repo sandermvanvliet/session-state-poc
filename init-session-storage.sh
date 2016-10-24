06686b9447434e5f5b2220def6e18dc119351861
Sander van Vliet <sander@codenizer.nl>
2016-10-24 16:25:09 +0200

    Makes the session storage work

--- init-session-storage.sh
+++ init-session-storage.sh
@@ -3,4 +3,6 @@
 . runsql.sh
 
 runsql_on_all "CREATE DATABASE sessionstate"
+#runsql_on_all "DROP TABLE sessionstate.``data``"
+runsql_on_all "CREATE TABLE sessionstate.``data``(``id`` int not null auto_increment, value blob null, \`key\` varchar(36) not null unique, size int not null, expiry_date datetime not null, lifetime time not null, is_slidingexpiry bit not null, primary key (``id``))"
 runsql_on_all "GRANT ALL PRIVILEGES ON sessionstate.* TO sessionStateUser@'%' IDENTIFIED BY 'aaabbb'"
--- scripts/slavetest-01.sh
+++ scripts/slavetest-01.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker exec -ti sessionstatepoc_mysql01_1 mysql -uroot -p123456 -vvv -e"USE repltest; SELECT * FROM foo;"
+docker exec -ti sessionstatepoc_mysql01_1 mysql -uroot -p123456 -vvv -e"USE sessionstate; SELECT * FROM data;"
